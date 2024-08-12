3f77d82f3292a30ce6171972a79503a9d5f7e781
kimdayeon37 <kdy37912@naver.com>
2024-08-13 00:04:08 +0900

    feat: movie:id route

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<Detail />} />
-      </Routes>
+        <Route path="/movie/:id" element={<Detail />} />
+        </Routes>
     </Router>
   );
 }
