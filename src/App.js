4993eb8f5d0071a21b1b09400828a30a508b17f4
kimdayeon37 <kdy37912@naver.com>
2024-08-13 13:15:01 +0900

    fix: route 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import RateList from "./routes/RateList";
 import Detail from "./routes/Detail";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ratelists" element={<RateList />} />
         <Route path="/movie/:id" element={<Detail />} />
         </Routes>
     </Router>
