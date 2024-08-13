c5c55fab78758e4b6dc983c556bb51afa9b35774
kimdayeon37 <kdy37912@naver.com>
2024-08-13 14:26:28 +0900

    design. 홈화면 버튼 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
 import styles from "./Home.module.css";
+import styled from "@emotion/styled";
 
 export default function Home() {
+  const Button = styled.button`
+    background: red;
+    color: black;
+    padding: 1em 2em;
+    font-size: 2em;
+    border-radius: 6px;
+    font-weight: bold;
+    &:hover {
+      border-color: red;
+      box-shadow: 0 0.5em 0.5em -0.4em white;
+    }
+  `;
   return (
-    <div className={styles.body}>
+    <div className={`${styles.body} flex-col`}>
       <div
         className="bg-cover bg-center w-full h-screen"
         style={{
@@ -13,6 +26,7 @@ export default function Home() {
       >
         <div className={styles.bgImg}></div>
       </div>
+      <Button>넷플릭스를 즐겨보세요 !</Button>
     </div>
   );
 }
