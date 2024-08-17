e4be7e8b733fa3b78b1db51a580dfa1ce0a6877e
kimdayeon37 <kdy37912@naver.com>
2024-08-17 15:52:30 +0900

    style: Dialog size, rating

--- src/components/common/Detail.js
+++ src/components/common/Detail.js
@@ -3,6 +3,7 @@ import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../core/Config";
 import MainImage from "../Movies/MainImage";
 import StarRating from "./StarRating";
+import styles from "../common/SlideCard.module.css"
 
 export default function Detail({ movieId, onClose }) {
   // const [open, setOpen] = useState(true);
@@ -20,17 +21,17 @@ export default function Detail({ movieId, onClose }) {
         console.log(response);
       });
   }, []);
-  // console.log(Movie.backdrop_path);
   if (!Movie) return null;
+  let voteAverage = Movie.vote_average.toFixed(1);
   return (
     <Dialog open={true} onClose={onClose} className="relative z-10">
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relsative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            className="relsative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-5xl data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <div className="bg-black px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="bg-black px-4 pb-4 pt-5">
               <MainImage
                 image={`${IMAGE_BASE_URL}original${Movie.backdrop_path}`}
                 title={Movie.original_title}
@@ -41,19 +42,23 @@ export default function Detail({ movieId, onClose }) {
                     as="h3"
                     className="text-base font-semibold leading-6 text-white"
                   >
-                    <div className="flex space-x-2">
-                      • {Movie.release_date} &nbsp; • {Movie.runtime}m &nbsp; •{" "}
-                      {Movie.genres &&
+                    <div className="flex space-x-2 ">
+                      <span className={styles.info}>{Movie.release_date}</span>
+                      <span className={styles.info}>{Movie.runtime}m</span>
+                      <span className={styles.info}>
+                      <div className="flex space-x-2">
+                        {Movie.genres &&
                         Movie.genres.map((g, index) => (
                           <p key={g.id}>
                             {g.name}
                             {index < Movie.genres.length - 1 && ","}
                           </p>
-                        ))}
+                        ))}</div>
+                        </span>
                     </div>
 
                     <br />
-                    <StarRating voteAverage={Movie.vote_average}/>
+                    <StarRating voteAverage={voteAverage}/>
                   </DialogTitle>
                   <div className="mt-2">
                     <p className="text-sm text-white">{Movie.overview}</p>
