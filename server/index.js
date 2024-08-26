666be1eeea75aa34517f6e4256e0e0ac2a4e2441
kimdayeon37 <kdy37912@naver.com>
2024-08-26 17:15:28 +0900

    feat: 비밀번호 암호화

--- server/index.js
+++ server/index.js
@@ -35,6 +35,42 @@ app.post('/api/users/register', async (req, res) => {
     }
 })
 
+// 로그인
+app.post("/api/users/login", (req, res) => {
+    // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+    User.findOne(
+      {
+        email: req.body.email,
+      },
+      (err, user) => {
+        if (!user) {
+          return res.json({
+            loginSuccess: false,
+            message: "이메일에 해당하는 유저가 없습니다.",
+          });
+        }
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (!isMatch) {
+            return res.json({
+              loginSuccess: false,
+              message: "비밀번호가 틀렸습니다.",
+            });
+          }
+          // 비밀번호까지 맞다면 토큰 생성
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, 세션 등등
+            res
+              .cookie("x_auth", user.token)
+              .status(200)
+              .json({ loginSuccess: true, userId: user._id });
+          });
+        });
+      }
+    );
+  });
+  
 
 const port = 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose"); // 몽구스를 가져온다
+const bcrypt = require("bcrypt"); // 비밀번호를 암호화
+const saltRounds = 10; // salt를 몇 글자로 할지
+const jwt = require("jsonwebtoken"); // 토큰 생성
+
 
 const userSchema = mongoose.Schema({
   name: {
@@ -32,6 +36,26 @@ const userSchema = mongoose.Schema({
   },
 });
 
+// 몽구스에서 가져온 메서드
+
+userSchema.pre("save", function (next) {
+  // save하기 전에 비밀번호를 암호화 시킨다
+  const user = this;
+  if (user.isModified("password")) { // 비밀번호를 바꿀 때만 암호화 시킨다.
+    // salt를 이용해서 비밀번호 암호화한다
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      // hash(plain 비밀번호, salt, 암호화된 비밀번호 함수)
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash; // 암호화된 비밀번호 생성하면 교체
+        next(); 
+      });
+    });
+  } else {
+    next();
+  }
+});
 const User = mongoose.model("User", userSchema); // 스키마를 모델로 감싸준다
 
 module.exports = { User }; // 다른 곳에서도 사용할 수 있도록 export
