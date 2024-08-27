9f633da8e0c8ca04ae3b5db14dc46a31adf1f42a
kimdayeon37 <kdy37912@naver.com>
2024-08-27 17:49:04 +0900

    style: 로그아웃 버튼

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -28,7 +28,7 @@ function Header(toSearch) {
   };
   return (
     <div>
-      <div className="grid grid-cols-8 gap-8 items-center p-4 bg-black">
+      <div className="grid grid-cols-8 gap-16 items-center p-4 bg-black">
         {/* 로고 */}
         <div
           className="col flex items-center"
@@ -55,18 +55,18 @@ function Header(toSearch) {
           <Link to="/tv" className="hover:underline">
             TV
           </Link>
-          {/* 검색 탭 */} 
-          <form class="max-w-lg" onSubmit={onSubmit}>
-          <label
-              for="default-search"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          {/* 검색 탭 */}
+          <form className="max-w-lg" onSubmit={onSubmit}>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
             >
               검색
             </label>
-            <div class="relative">
-              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
-                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -74,9 +74,9 @@ function Header(toSearch) {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
                 </svg>
@@ -84,13 +84,13 @@ function Header(toSearch) {
               <input
                 type="search"
                 id="default-search"
-                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
                 placeholder="Search Movies.."
                 required
               />
               <button
                 type="submit"
-                class="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                className="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               >
                 검색
               </button>
@@ -98,9 +98,13 @@ function Header(toSearch) {
           </form>
         </div>
         {/* 로그아웃 버튼 */}
-        <button onClick={handleClick} className={btn.okButton}>로그아웃</button>
+        <div
+          onClick={handleClick}
+          className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl py-2 px-4 flex items-center justify-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          로그아웃
+        </div>
       </div>
-
     </div>
   );
 }
