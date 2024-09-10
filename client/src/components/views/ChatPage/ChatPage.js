6cab831b74a62bc35d9a22fb9cac4e5ab20779d1
kimdayeon37 <kdy37912@naver.com>
2024-09-10 11:37:55 +0900

    fix: redux data로 name 전환

--- client/src/components/views/ChatPage/ChatPage.js
+++ client/src/components/views/ChatPage/ChatPage.js
@@ -3,14 +3,30 @@ import styles from "./ChatPage.module.css";
 import socket from "../../../server";
 import InputField from "./InputField/InputField";
 import MessageContainer from "./MessageContainer/MessageContainer";
+import Auth from "../../../hoc/auth"
+import { useSelector } from "react-redux";
 
-export default function ChatPage() {
+function ChatPage() {
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState([])
-  const isNameAskedRef = useRef(false);
+  const isNameAskedRef = useRef(false); // strict mode이슈로 인한 참조변수 사용
+  const userD = useSelector((state) => state.user);
 
   useEffect(() => {
+    // Redux 상태의 생명주기와 소켓 연결 시점의 차이로 useEffect 조건부 실행
+    if (userD.userData && !isNameAskedRef.current) {
+      const userName = userD.userData.name;
+      console.log("Emitting login event with userName:", userName);
+      socket.emit("login", userName, (res) => {
+        console.log("Login response:", res);
+        if (res?.ok) {
+          setUser(res.data);
+        }
+      });
+      isNameAskedRef.current = true; // 한번 렌더링 되면 더이상 안되게끔
+    }
+
     const handleMessage = (message) => {
       console.log(message);
       setMessageList((prevState) => [...prevState, message]);
@@ -18,26 +34,12 @@ export default function ChatPage() {
   
     socket.on("message", handleMessage);
   
-    if (!isNameAskedRef.current) {
-      askUserName();
-      isNameAskedRef.current = true;
-    }
-  
     return () => {
       socket.off("message", handleMessage);
     };
-  }, []); // 빈 의존성 배열
+  }, [userD.userData]); // 빈 의존성 배열
   
-  const askUserName = () => {
-    const userName = localStorage.getItem("userId")
-    console.log(userName);
-    socket.emit("login", userName, (res) => {
-      console.log(res);
-      if (res?.ok) {
-          setUser(res.data)
-      }
-    });
-  };
+
   const sendMessage = (event) => {
     event.preventDefault();
     socket.emit("sendMessage", message, (res) => {
@@ -56,3 +58,5 @@ export default function ChatPage() {
     </div>
   );
 }
+
+export default Auth(ChatPage, true);
